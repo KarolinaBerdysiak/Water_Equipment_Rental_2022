f043fd8360d468e7f642dfbd413c53e460d0a58b
KAROLINA BERDYSIAK <94194893+KarolinaBerdysiak@users.noreply.github.com>
2022-03-20 11:15:32 +0100

    Initial commit

---
+++ src/main/java/com/example/water_equipment_rental_2022/WaterEquipmentRental2022Application.java
@@ -0,0 +1,13 @@
+package com.example.water_equipment_rental_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterEquipmentRental2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterEquipmentRental2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/water_equipment_rental_2022/WaterEquipmentRental2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.water_equipment_rental_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterEquipmentRental2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
